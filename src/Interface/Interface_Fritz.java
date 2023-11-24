211289c9d27c47f8829a7e63137b7f46d49737a5
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2023-11-24 16:53:45 +0800

    Added Interface

--- src/Interface/Interface_Fritz.java
+++ src/Interface/Interface_Fritz.java
@@ -4,9 +4,9 @@ package Interface;
 
 public class Interface_Fritz extends Interface_Student{
          public void Gwapo(){
-              System.out.println("Si Fritz ay kasing gwapo ni jomar");
+              System.out.println("Si Fritz ay mabait yan");
           }
           public void Code(){
-              System.out.println("Si Fritz ay code God");
+              System.out.println("Si Fritz Marunong mag code");
           }
 }
